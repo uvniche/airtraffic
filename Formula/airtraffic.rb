972d471490d0d97e25183592ac769a881f652189
avaneesh <uvniche@icloud.com>
2026-09-04 11:06:34 +0530

    Install AirTraffic from rolling release

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -1,14 +1,16 @@
 class Airtraffic < Formula
   desc "Track per-app network data usage on macOS"
   homepage "https://github.com/uvniche/airtraffic"
+  url "https://github.com/uvniche/airtraffic/releases/download/latest/airtraffic-macos-arm64.tar.gz"
+  version "1.0.0"
+  sha256 "bc47acc8c9b9e796bd3dee755436c30209c125a0be3573b04b3f453600353fce"
   license "MIT"
-  head "https://github.com/uvniche/airtraffic.git", branch: "main"
 
+  depends_on arch: :arm64
   depends_on macos: :ventura
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/airtraffic"
+    bin.install "airtraffic"
   end
 
   test do
