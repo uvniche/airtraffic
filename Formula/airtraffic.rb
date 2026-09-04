35b0cc560cea01b663fd28c65aa5eb55b6c46603
avaneesh <uvniche@gmail.com>
2026-09-04 11:42:38 +0530

    Distribute universal macOS binary

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -1,18 +1,16 @@
 class Airtraffic < Formula
   desc "Track per-app network data usage on macOS"
   homepage "https://github.com/uvniche/airtraffic"
-  url "https://github.com/uvniche/airtraffic.git",
-      revision: "81ea8858da7a4841ea6401082495fa871e96d5f0"
+  url "https://github.com/uvniche/airtraffic/releases/download/rolling/airtraffic-macos-universal.tar.gz"
   version "1.0.0"
+  sha256 "29166d988fbb9f818c7376002c283745ecb60cf221eb5e87a1aaba3a82203ddf"
   license "MIT"
-  revision 1
+  revision 2
 
-  depends_on xcode: ["15.0", :build]
   depends_on macos: :ventura
 
   def install
-    system "xcrun", "swift", "build", "--configuration", "release", "--disable-sandbox"
-    bin.install ".build/release/airtraffic"
+    bin.install "airtraffic"
   end
 
   test do
