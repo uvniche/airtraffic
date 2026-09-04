2894accccf7ffe908c4422c84f644246d7955702
avaneesh <uvniche@icloud.com>
2026-09-04 17:01:39 +0530

    Harden rolling release build

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -3,9 +3,9 @@ class Airtraffic < Formula
   homepage "https://github.com/uvniche/airtraffic"
   url "https://github.com/uvniche/airtraffic/releases/download/rolling/airtraffic-macos-arm64.tar.gz"
   version "1.0.0"
-  sha256 "3f1884f76dc5aaae21a529aeaf52f974bc78c39b08ab0f110efce6062307a240"
+  sha256 "f9ef62cd51c5183c34f297d767ef258570c8b1193a4a9556e3e63644304d800a"
   license "MIT"
-  revision 4
+  revision 5
 
   depends_on arch: :arm64
   depends_on macos: :ventura
