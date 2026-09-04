358a47531f89442c32659b48d5ca440a8b0c8598
avaneesh <uvniche@icloud.com>
2026-09-04 11:36:10 +0530

    Add Intel Mac installation support

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -1,16 +1,18 @@
 class Airtraffic < Formula
   desc "Track per-app network data usage on macOS"
   homepage "https://github.com/uvniche/airtraffic"
-  url "https://github.com/uvniche/airtraffic/releases/download/rolling/airtraffic-macos-arm64.tar.gz"
+  url "https://github.com/uvniche/airtraffic.git",
+      revision: "81ea8858da7a4841ea6401082495fa871e96d5f0"
   version "1.0.0"
-  sha256 "bc47acc8c9b9e796bd3dee755436c30209c125a0be3573b04b3f453600353fce"
   license "MIT"
+  revision 1
 
-  depends_on arch: :arm64
+  depends_on xcode: ["15.0", :build]
   depends_on macos: :ventura
 
   def install
-    bin.install "airtraffic"
+    system "xcrun", "swift", "build", "--configuration", "release", "--disable-sandbox"
+    bin.install ".build/release/airtraffic"
   end
 
   test do
