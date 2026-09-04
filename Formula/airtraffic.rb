beb8b9307a32e8378855670efb8e567c365282f3
avaneesh <uvniche@icloud.com>
2026-09-04 15:57:04 +0530

    Update ARM64 release checksum

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -3,7 +3,7 @@ class Airtraffic < Formula
   homepage "https://github.com/uvniche/airtraffic"
   url "https://github.com/uvniche/airtraffic/releases/download/rolling/airtraffic-macos-arm64.tar.gz"
   version "1.0.0"
-  sha256 "bc47acc8c9b9e796bd3dee755436c30209c125a0be3573b04b3f453600353fce"
+  sha256 "e3d783d3c8c3f2545dfb09b91e1f74cf764cc131f03e4da7106fc18be674771f"
   license "MIT"
   revision 3
 
