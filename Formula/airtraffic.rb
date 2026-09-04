5bea2343102788cd8cdee20697819183a2dc4a08
avaneesh <uvniche@gmail.com>
2026-09-04 11:08:40 +0530

    Rename rolling release to main

--- Formula/airtraffic.rb
+++ Formula/airtraffic.rb
@@ -1,7 +1,7 @@
 class Airtraffic < Formula
   desc "Track per-app network data usage on macOS"
   homepage "https://github.com/uvniche/airtraffic"
-  url "https://github.com/uvniche/airtraffic/releases/download/latest/airtraffic-macos-arm64.tar.gz"
+  url "https://github.com/uvniche/airtraffic/releases/download/main/airtraffic-macos-arm64.tar.gz"
   version "1.0.0"
   sha256 "bc47acc8c9b9e796bd3dee755436c30209c125a0be3573b04b3f453600353fce"
   license "MIT"
